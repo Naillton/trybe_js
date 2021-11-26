e26da04ec3b3811c2f87acd6c18d7416b1910629
Naillton <Nailton_junior@protonmail.com>
2021-11-26 12:34:57 -0600

    Primeiros codigos js

---
+++ script.js
@@ -0,0 +1,12 @@
+const myName = "Nailton";
+
+const birthCity = "Caruaru";
+
+//birthCity = "Belo jardim"; nao podemos mudar o valor de uma constante pois as mesmas sao constantes no codigo
+
+let birthYear = 2000;
+
+birthYear = 2030;
+console.log(birthYear)
+
+console.log("Meu nome e "+myName+" Nasci em "+birthCity+" no ano "+birthYear);
