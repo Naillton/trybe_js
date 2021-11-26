adc88fc2d70c84284b801a7b924cb086b979f380
Naillton <Nailton_junior@protonmail.com>
2021-11-26 13:08:43 -0600

    Operadores js

---
+++ typeof.js
@@ -0,0 +1,22 @@
+let patientId = 50;
+patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+//console.log(typeof(patientAge));
+//console.log(typeof(patientInfo));
+//console.log(typeof(patientEmai));
+//console.log(typeof(isEnrolled));
+//console.log(typeof(patientId));
+
+const base = 5;
+let height = 8;
+const area = base * height;
+//console.log(area);
+
+const perimeter = area + area + area + area;
+console.log(perimeter);
