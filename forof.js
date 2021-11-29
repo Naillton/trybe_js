4cbdf374b2c7d087834056f0da8661255ce802ca
Naillton <Nailton_junior@protonmail.com>
2021-11-29 15:40:35 -0600

    for of

---
+++ forof.js
@@ -0,0 +1,25 @@
+//for in percorre objetos interaveis, com ele se formos contar letra por letra as variaveis temos o resultado como numeros indicando o index
+//for of percorre objetos interativos, com o modo interativo com for of conseguiremos pegar o valor de cada letra e imprimir na tela
+
+// let numeros = [1,2,3,4,5,6];
+// for(let numero of numeros){
+//     console.log(numero+"\n");
+// }
+
+// let word = 'Hello';
+// for(letter of word){
+//     console.log(letter);
+// }
+
+// let arroOfNumbers = [10, 20, 30];
+
+// for(let sum of arroOfNumbers){
+//     sum += 1;
+//     console.log(sum);
+// }
+
+let names = ['Joao', 'Maria', 'Antonio', 'Margarida'];
+
+for(let imprimir of names){
+    console.log(imprimir);
+}
