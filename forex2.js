d93a2e0eae55a988217077a443f364a0ac189c17
Naillton <Nailton_junior@protonmail.com>
2021-11-29 15:19:51 -0600

    usando o for para contagem em array

---
+++ forex2.js
@@ -0,0 +1,29 @@
+// let numero = 7;
+
+// for(let contador = 0; contador <= 10; contador++){
+    
+//     let resultado = numero * contador;
+
+//     console.log(numero+" * "+contador+" = "+resultado+"\n");
+// }
+
+// let nomes = ['Marcos', 'Joana', 'Flavio'];
+
+// for(let cont = 0; cont < nomes.length; cont++){
+
+//     console.log("Boas vindas a Trybe "+nomes[cont]+"\n");
+// }
+
+//com while
+// let groceryList = ['Arroz', 'Feijao', 'Alface', 'Melancia'];
+// let contador = 0;
+// while(contador < groceryList.length){
+//     console.log(groceryList[contador]);
+//     contador++
+// }
+
+ let groceryList = ['Arroz', 'Feijao', 'Alface', 'Melancia'];
+
+for(let contador = 0; contador < groceryList.length; contador+=1){
+     console.log(groceryList[contador])
+ }
