9087a14aee1df80a7ed0bbea0f698785854c575b
Naillton <Nailton_junior@protonmail.com>
2021-11-29 12:48:27 -0600

    exercicios array

---
+++ forex1.js
@@ -0,0 +1,12 @@
+let menu = ['Home', 'Servicos', 'Portifolio', 'Links'];
+let menuServices = menu[1];
+
+//console.log(menuServices);
+
+let indexOfPortifolio = menu.indexOf('Portifolio');
+
+//console.log(indexOfPortifolio);
+
+menu.push('Contato');
+
+console.log(menu);
