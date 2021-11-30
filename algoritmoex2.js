fde1c0e453898b92d798b33e96b8683c00557f7e
Naillton <Nailton_junior@protonmail.com>
2021-11-30 16:08:12 -0600

    desenhando triangulo com asterisco

---
+++ algoritmoex2.js
@@ -0,0 +1,14 @@
+ let n = 5;
+let ast = '';
+for(let i = 1; i <= 5; i++){
+    ast = ast+'*';
+    console.log(ast);
+}
+
+//com o exemplo abaixo escrevo um asterisco a mais
+/* let n = 5;
+let ast = '*';
+for(let i = 1; i < n; i++){
+    ast = ast+'*';
+    console.log(ast)
+} */
