3fbc7bdb406c913b445fa555b3393ec6ff95bc24
Naillton <Nailton_junior@protonmail.com>
2021-12-01 15:32:20 -0600

    for in e for of, diferenca e uso

---
+++ forin.js
@@ -0,0 +1,34 @@
+/* let pizzasDoces = {
+    sabor: 'Calabresa',
+    preco: 24.90,
+    bordaChedar: true
+}
+for(let key in pizzasDoces){
+    console.log(key+": "+pizzasDoces[key]);
+} */
+
+/* let pizzas = ['Calabresa', 'Frango', 'Mussarela'];
+for(let key in pizzas){
+    console.log(key+": "+pizzas[key]);
+} */
+
+/* let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+} */
+
+/* let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+} */
+//com o for of percorremos objetos iteraveis
+/* let food = ['hamburger', 'bife', 'acaraje'];
+for(let index in food){
+    console.log(index);
+} */
