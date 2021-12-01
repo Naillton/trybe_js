043f797c27aadb09137cff70e17f0f0b5a9ecaed
Naillton <Nailton_junior@protonmail.com>
2021-11-30 18:52:18 -0600

    exercicio 3

---
+++ algoritmoex.3.js
@@ -0,0 +1,35 @@
+/* let n = 5;
+let ast = '';
+let space = '';
+for(let i = 0; i < 5; i++){
+    ast = ast+'*';
+    if(ast == '*'){
+    console.log("    "+ast);
+    }else if(ast == '**'){
+        console.log("   "+ast);
+    }else if(ast == '***'){
+        console.log("  "+ast);
+    }else if(ast == '****'){
+        console.log(" "+ast);
+    }else if(ast == '*****'){
+        console.log(ast);
+    }
+    
+} */
+
+//depois de muita luta e de tirar duvidas na mentoria consegui fazer a questao com menos codigo
+/* let n = 5;
+let space = "";
+let ast = "";
+for(let i = 0; i < 5; i++){
+    for(let s = 0; s <= n; s+=1){
+        if(s < n){
+            space +=" ";
+        }else{
+            ast +="*"
+        }
+    }
+    console.log(space+ast);
+    space = "";
+    n--;
+} */
--- algoritmoex2.js
+++ algoritmoex2.js
@@ -1,4 +1,4 @@
- let n = 5;
+let n = 5;
 let ast = '';
 for(let i = 1; i <= 5; i++){
     ast = ast+'*';
