1c616edb2bdc1301d997d9255b7b68d552ee2061
Naillton <Nailton_junior@protonmail.com>
2021-12-01 18:01:08 -0600

    mini calc

---
+++ calcDoisNum.js
@@ -0,0 +1,23 @@
+/* function calc(num1, num2, operation){
+	if(operation === '+'){
+		resultado = num1 + num2;
+		return "O resultado de "+num1+" + "+num2+" e ="+resultado;
+	}else if(operation === '-'){
+		resultado = num1 - num2;
+		return "O resultado de "+num1+" - "+num2+" e ="+resultado;
+	}else if(operation === '/'){
+		resultado = num1 / num2;
+		return "O resultado de "+num1+" / "+num2+" e ="+resultado;
+	}else if(operation === '*'){
+		resultado = num1 * num2;
+		return "O resultado de "+num1+" * "+num2+" e ="+resultado;
+	}else if(operation === '%'){
+		resultado = num1 % num2;
+		return "O resultado de "+num1+" % "+num2+" e ="+resultado;
+	}else{
+		return "Operador invalido informe um correto"
+	}
+
+}
+
+console.log(calc(50, 2, '*')); */
