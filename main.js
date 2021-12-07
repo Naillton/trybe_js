9c14921bb2769b3055edcbc67130f65c7931d3fe
Naillton <Nailton_junior@protonmail.com>
2021-12-07 15:34:53 -0600

    eventos main.js

---
+++ main.js
@@ -0,0 +1,62 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+const header = document.querySelector('h1')
+
+// 1. Copie esse arquivo e edite apenas ele;
+// 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
+// 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+//funcao para adcionar classe a primeiroa li
+function add(){
+    let addClass = document.querySelector('li')
+    addClass.className = 'tech'
+}
+
+//funcao para alterar o valor do texto do bloco verde
+function alterText(){
+    firstLi.innerText = input.value;
+}
+//evento que chama a funcao alterText e a altera
+input.addEventListener("change", alterText);
+
+//funcao para clicar duas vezes no texto e alterar a cor e ir para o meu portifolio
+function alter(){
+    header.style.color = 'red'
+}
+//evento para definir mudanca de cor
+header.addEventListener("dblclick", alter);
+
+function mouseDeslize(){
+    myWebpage.style.color = 'yellow'
+}
+myWebpage.addEventListener("mouseover", mouseDeslize);
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
