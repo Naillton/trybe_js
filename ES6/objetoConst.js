17ec1e9f5d37cdb29bec19dffc65372c1aa3191a
Naillton <Nailton_junior@protonmail.com>
2022-01-12 12:50:37 -0600

    objetos const

--- ES6/objetoConst.js
+++ ES6/objetoConst.js
@@ -10,3 +10,11 @@ const userInfo = {
   console.log(userInfo); // { name: 'João', id: '5489-2', email: 'claudio@email.com' }
   //com constantes objetos, conseguimos mudar o valor setando a opcao que queremos como por exemplo userInfo.name
   //se tentarmos mudar todo o objeto ele retornara um erro
+
+  //O mesmo tambem acontece com constantes arrays
+  const technologies = ['Javascript', 'CSS', 'HTML'];
+  technologies.push('React');
+  console.log(technologies); // [ 'Javascript', 'CSS', 'HTML', 'React' ]
+
+  /* technologies = ['Javascript', 'CSS', 'HTML', 'React']
+  console.log(technologies); // Erro */
