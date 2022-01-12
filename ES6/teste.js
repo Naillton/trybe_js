a14c224cf36d32ccdee050c3be4e6c9210322a35
Naillton <Nailton_junior@protonmail.com>
2022-01-12 12:46:08 -0600

    diferencas entre const e let em exemplo

---
+++ ES6/teste.js
@@ -0,0 +1,9 @@
+const favoriteLanguage = 'Javascript';
+favoriteLanguage = 'Python';
+console.log(favoriteLanguage); // Erro
+//como foi declarada uma constante favoriteLanguage nos nao temos como mudar o seu valore por isso gera um erro na hora do console.log
+
+let favoriteTechnology = 'Machine learning';
+favoriteTechnology = 'Facial recognition';
+console.log(favoriteTechnology); // Facial recognition
+//com uma variavel let usada , conseguimos mudar o valor e ter como resultado Facial recognition
