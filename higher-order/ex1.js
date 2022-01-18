07a4e7cc48b9ba7b431988b0e85f0832a7fbf94c
Naillton <Nailton_junior@protonmail.com>
2022-01-18 12:58:08 -0600

    exemplos pratica

---
+++ higher-order/ex1.js
@@ -0,0 +1,9 @@
+const acorda = () =>  'Acordando!!';
+
+const cafe = () => 'Bora tomar cafe!!';
+
+const dormir = () => 'Partiu dormir!!';
+
+const doingThings = (result) => result();
+
+console.log(doingThings(dormir));
