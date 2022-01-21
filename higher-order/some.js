64ebdd307af151b051bc3ae3df754c5c1fb528c3
Naillton <Nailton_junior@protonmail.com>
2022-01-21 08:32:41 -0600

    func some

--- higher-order/some.js
+++ higher-order/some.js
@@ -18,8 +18,17 @@ const verifyGrades = (studentGrades) => (
     Object.values(studentGrades).every((grade) => grade === 'Aprovado')
 );
 
-console.log(verifyGrades(grades));
+//console.log(verifyGrades(grades));
 
 //ja no nosso exemplo com o every(), temos um objeto e uma constante de verificacao
 //onde com o Object.values eu pego todos os valores dos objetos e uso o every() para verificar se
 //todos os valores sao aprovados se sim retorna true mas se um unico valor for reprovado retorna false
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  const result = arr.some((array) => array === name);
+  return result;
+}
+
+console.log(hasName(names, 'Ana'))
