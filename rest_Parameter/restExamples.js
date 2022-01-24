8375f3516f460c0fff5af61056e5f49ed742150e
Naillton <Nailton_junior@protonmail.com>
2022-01-24 12:47:21 -0600

    exemples rest

---
+++ rest_Parameter/restExamples.js
@@ -0,0 +1,11 @@
+/* O parametro rest ira te auxiliar a criar funcoes que recebem um numero ilimitado de argumentos
+assim suas funcoes ficam mais flexiveis, os argumentos acessados no parametro sao salvos em um array
+que podem ser acessados de dentro da funcao vamos para um exemplo */
+
+function params(...args) {
+    console.log('parametros:', args);
+    return `Voce passou ${args.length} parametros para a funcao`;
+};
+
+console.log(params(0, 1, 2));
+console.log(params('string', null, [1, 2, 4], {}));
