b0cac58913d69e592ad717fe68c1233d4d19c2a0
Naillton <Nailton_junior@protonmail.com>
2022-01-24 12:17:57 -0600

    spread em array de strings

--- spread/spread.js
+++ spread/spread.js
@@ -7,7 +7,7 @@ const numbers1to10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const numbers1to20 = [...numbers1to10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-console.log(numbers1to20);
+//console.log(numbers1to20);
 
 //Agora temos dois arrays diferentes onde preservamos os valores do primeiro e adcionamos os outros valores
 //Podemos fazer a mesma coisa com objetos onde os valores serao inseridos da mesma forma;
@@ -36,6 +36,27 @@ const personDeveloper = {
 
 const personDeveloperTrybe = {...personCarachter, ...personDeveloper};
 
-console.log(personDeveloperTrybe);
+//console.log(personDeveloperTrybe);
 
 //como fizemos no exercicio anterior usar o spread e bem simples mas requer muita atencao
+
+const numbers = [1, 2, 3, 4];
+
+const numeros = [...numbers, 5, 6, 7];
+
+console.log(numeros);
+
+//mais uma vez utilizamos o spread para modificar e atualizar um array
+
+//Agora vamos usar o spread para unir um array de strings
+
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+
+console.log(months);
+
+//com o spread conseguimos adcionar arrays em um array month para mostrar os meses do ano
