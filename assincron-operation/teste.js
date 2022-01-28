60ef6e486373694ec83aacebeaf1041c30386ffb
Naillton <Nailton_junior@protonmail.com>
2022-01-28 11:41:20 -0600

    testes assincronos

---
+++ assincron-operation/teste.js
@@ -0,0 +1,22 @@
+/* const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers);
+ */
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout( () => console.log(numbers), 3000); // O retorno é [2, 3]
+
+// nao onseguimos imprimir o 1 com o console.log() normal pois o mesmo e sincrono e logo que lido ja executa
+// se colocarmos o console.log() dentro de um Timeout conseguimos reproduzir o codigo
