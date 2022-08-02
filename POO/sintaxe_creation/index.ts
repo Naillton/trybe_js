a4dba0f21ee40797faf90f1c249d74772817eaf0
Naillton <Nailton_junior@protonmail.com>
2022-08-02 15:27:52 -0300

    typescript

---
+++ POO/sintaxe_creation/index.ts
@@ -0,0 +1,24 @@
+class Person {
+  name: string;
+  height: number;
+  weight: number;
+
+  constructor(n: string, h: number, w: number) {
+    console.log(`creating person ${n}`);
+    this.name = n;
+    this.height = h;
+    this.weight = w;
+  }
+
+  public sllep() {
+    console.log(`${this.name}: sheep sheep sheep zzzzzzz`);
+  }
+}
+
+// instancia de classe person para criacao dos obj p1 e p2
+const p1 = new Person('Maria', 171, 58);
+const p2 = new Person('João', 175, 66);
+console.log(p1.name, p1.height, p1.weight);
+console.log(p2.name, p2.height, p2.weight);
+p1.sllep();
+p2.sllep();
---
+++ POO/sintaxe_creation/tvClass.ts
@@ -0,0 +1,37 @@
+class TV {
+  brand: string;
+  size: number;
+  resolution: string;
+  connections: string;
+
+  constructor(
+    b: string,
+    s: number,
+    r: string,
+    c: string,
+  ) {
+    this.brand = b;
+    this.size = s;
+    this.resolution = r;
+    this.connections = c;
+  }
+
+  public turnOn() {
+    console.log(`
+      VENHA COMPRAR JÁ A TV
+      ${this.brand}
+      ${this.size}
+      ${this.resolution}
+      ${this.connections}
+    `);
+  };
+}
+
+const tv1 = new TV(
+  'Samsung',
+  48,
+  '4K',
+  'HDMI'
+);
+
+tv1.turnOn();
